2b6aac98d0b0fc5203e428d244b4085abef233e9
SEVER <safregs@gmail.com>
2026-09-01 00:23:18 +0300

    Delete src directory

--- src/shared/index.ts
+++
@@ -1,5 +0,0 @@
-export { Header } from './components/Header';
-export { ToastContainer } from './components/ToastContainer';
-export { Confetti } from './components/Confetti';
-export { StatsModal } from './components/StatsModal';
-export { HowToModal } from './components/HowToModal';
